79ec2c5af195c1cc00b6d77e90133677822a3608
0xABADCAFE <karlchurchill@gmail.com>
2020-07-17 19:07:50 +0100

    Split LFO into Fixed and Controlled, the latter using signal buffers

---
+++ src/oscillator/control/ControlledLFO.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ *      _/_/_/  _/_/_/  _/      _/  _/_/_/      _/_/_/
+ *   _/          _/    _/_/  _/_/  _/    _/  _/
+ *  _/  _/_/    _/    _/  _/  _/  _/_/_/      _/_/
+ * _/    _/    _/    _/      _/  _/              _/
+ *  _/_/_/  _/_/_/  _/      _/  _/        _/_/_/
+ *
+ *  - Grossly Impractical Modular PHP Synthesiser -
+ *
+ */
+
+declare(strict_types = 1);
+
+/**
+ * Control
+ */
+namespace ABadCafe\Synth\Oscillator\Control;
+use ABadCafe\Synth\Signal;
+use ABadCafe\Synth\Oscillator;
+use \SPLFixedArray;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class ControlledLFO extends Base {
+
+    use Signal\TContextIndexAware;
+
+    protected Signal\IGenerator $oGenerator;
+
+    protected Signal\Control\IStream
+        $oRateControl,
+        $oDepthControl
+    ;
+
+    protected Signal\Control\Packet
+        $oGeneratorInput,
+        $oLastOutput
+    ;
+    protected int $iSamplePosition = 0;
+
+    protected float
+        $fCurrentFrequency = ILimits::F_DEF_FREQ, // The present instantaneous frequency considering any pitch control
+        $fPhaseCorrection  = 0.0                  // The accumulated phase difference as a result of pitch control
+    ;
+
+
+    /**
+     * Constructor.
+     *
+     * @param Signal\IGenerator $oGenerator
+     * @param float             $fFrequency
+     * @param float             $fDepth;
+     */
+    public function __construct(
+        Signal\IGenerator      $oGenerator,
+        Signal\Control\IStream $oRateControl,
+        Signal\Control\IStream $oDepthControl
+    ) {
+        $this->oGenerator       = $oGenerator;
+        $this->oRateControl     = $oRateControl;
+        $this->oDepthControl    = $oDepthControl;
+        $this->oGeneratorInput  = new Signal\Control\Packet();
+        $this->oLastOutput      = new Signal\Control\Packet();
+        $this->fPhaseCorrection = 0;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getPosition() : int {
+        return $this->iSamplePosition;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getFrequency() : float {
+        return $this->fCurrentFrequency;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function reset() : self {
+        $this->iSamplePosition      = 0;
+        $this->fCurrentFreqency     = ILimits::F_DEF_FREQ;
+        $this->fPhaseCorrection     = 0;
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setFrequency(float $fFrequency) : self {
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setDepth(float $fDepth) : self {
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function emit(?int $iIndex = null) : Signal\Control\Packet {
+        if ($this->useLast($iIndex)) {
+            return $this->oLastOutput;
+        }
+
+        $oValues = $this->oGeneratorInput->getValues();
+        $oShifts = $this->oRateControl->emit($iIndex);
+
+        $fTimeStep     = Signal\Context::get()->getSamplePeriod() * $this->oGenerator->getPeriod();
+        foreach ($oShifts->getValues() as $i => $fNextFrequency) {
+            $fTime                   = $fTimeStep * $this->iSamplePosition++;
+            $oValues[$i]             = ($this->fCurrentFrequency * $fTime) + $this->fPhaseCorrection;
+            $this->fPhaseCorrection += $fTime * ($this->fCurrentFrequency - $fNextFrequency);
+            $this->fCurrentFrequency = $fNextFrequency;
+        }
+
+        $oOutput = $this->oGenerator->map($this->oGeneratorInput);
+        $oOutput->modulateWith($this->oDepthControl->emit($iIndex));
+        $this->oLastOutput = $oOutput;
+        return $oOutput;
+    }
+}
---
+++ src/oscillator/control/FixedLFO.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ *      _/_/_/  _/_/_/  _/      _/  _/_/_/      _/_/_/
+ *   _/          _/    _/_/  _/_/  _/    _/  _/
+ *  _/  _/_/    _/    _/  _/  _/  _/_/_/      _/_/
+ * _/    _/    _/    _/      _/  _/              _/
+ *  _/_/_/  _/_/_/  _/      _/  _/        _/_/_/
+ *
+ *  - Grossly Impractical Modular PHP Synthesiser -
+ *
+ */
+
+declare(strict_types = 1);
+
+/**
+ * Control
+ */
+namespace ABadCafe\Synth\Oscillator\Control;
+use ABadCafe\Synth\Signal;
+use ABadCafe\Synth\Oscillator;
+use \SPLFixedArray;
+use function ABadCafe\Synth\Utility\clamp;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class FixedLFO extends Base {
+
+    use Signal\TContextIndexAware;
+
+    protected float
+        $fFrequency        = ILimits::F_DEF_FREQ, // The base frequency
+        $fCurrentFrequency = ILimits::F_DEF_FREQ, // The present instantaneous frequency considering any pitch control
+        $fScaleVal         = 0.0,
+        $fDepth            = 0.5
+    ;
+
+    /**
+     * Constructor.
+     *
+     * @param Signal\IGenerator $oGenerator
+     * @param float             $fFrequency
+     * @param float             $fDepth;
+     */
+    public function __construct(
+        Signal\IGenerator $oGenerator,
+        float             $fFrequency  = ILimits::F_DEF_FREQ,
+        float             $fDepth      = 0.5
+    ) {
+        parent::__construct($oGenerator);
+        $this->setFrequency($fFrequency);
+        $this->fPhaseCorrection = 0;
+        $this->fDepth       = $fDepth;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getPosition() : int {
+        return $this->iSamplePosition;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getFrequency() : float {
+        return $this->fFrequency;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function reset() : self {
+        $this->iSamplePosition      = 0;
+        $this->fCurrentFreqency     = $this->fFrequency;
+        $this->fPhaseCorrection     = 0;
+        return $this;
+    }
+
+    /**
+     * Set the oscillator signal frequency
+     *
+     * @param  float $fFrequency
+     * @return self
+     */
+    public function setFrequency(float $fFrequency) : self {
+        $this->fFrequency = clamp($fFrequency, ILimits::F_MIN_FREQ, ILimits::F_MAX_FREQ);
+        $this->fScaleVal  = $this->oGenerator->getPeriod() * $this->fFrequency * Signal\Context::get()->getSamplePeriod();
+        $this->fCurrentFreqency = $this->fFrequency;
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @param  float $fDepth
+     * @return self
+     */
+    public function setDepth(float $fDepth) : self {
+        $this->fDepth = $fDepth;
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function emit(?int $iIndex = null) : Signal\Control\Packet {
+        if ($this->useLast($iIndex)) {
+            return $this->oLastOutput;
+        }
+
+        $oValues = $this->oGeneratorInput->getValues();
+
+        // Basic linear intervals, there is no phase adjustment
+        foreach ($oValues as $i => $fValue) {
+            $oValues[$i] = $this->fScaleVal * $this->iSamplePosition++;
+        }
+
+        $oOutput = $this->oGenerator->map($this->oGeneratorInput);
+        $oOutput->scaleBy($this->fDepth);
+        $this->oLastOutput = $oOutput;
+        return $oOutput;
+    }
+}
