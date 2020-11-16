c6a1778b86a049769a4008274b2e6e18549b4428
0xABADCAFE <karlchurchill@gmail.com>
2020-11-16 18:13:09 +0000

    Waveshaper

--- doc/WaveShapers.md
+++ doc/WaveShapers.md
@@ -20,6 +20,30 @@ This _WaveShaper_ models a capacitave load on the output of a _Generator_. This 
 
 This _WaveShaper_ models the Feedback behaviour of classic FM synthesis. This uses an average of the most recent output values as a phase modification for the input of the next value. This_WaveShaper_ performs phase modification only. When applied to a basic sinewave _Generator_ the effect is to move the waveform towards a sawtooth. The strength of the effect is set on construction and cannot change.
 
+The effect of the _FixedPhaseFeedback_ _WaveShaper_ on a number of simple _Generator_ outputs in an _Oscillator_ at 440Hz, 44.1 kHz output are shown below.
+
+### 1. Sine
+As the intensity of the shaper is increased from 0.0 to 0.5 to 1.0, the original wave distored towards a sawtooth. This is the primary function of this technique in classic FM synthesis.
+
+![sine wave](./images/WaveShaperPhaseSine.png)
+
+### 2. Triangle
+As the triangle wave shape is superficially similar to the sine the effects are somewhat similar but distortion towards a sawtooth happens much more quickly. As the strength of the effect is increased, significantly more harmonics are added.
+
+![triangle wave](./images/WaveShaperPhaseTriangle.png)
+
+### 3. SawDown
+As the intensity of the shaper is increased from 0.0 to 1.0, the transition point from minimum to maximum becomes increasingly perturbed. At 0.5 the effect is somewhat similar to a resonant lowpass filter and at 1.0 many adds many additional harmonics.
+
+![saw wave](./images/WaveShaperPhaseSawDown.png)
+
+### 4. Square
+As the intensity of the shaper is increased, the crossover points become increasingly perturbed. As the square wave only has two amplitude states, this introduces a PWM effect reminiscent of early 8-bit consoles.
+
 ### FixedPhaseFeedbackWithCapacitance
 
 This _WaveShaper_ is a union of the _FixedPhaseFeedback_ and _FixedCapacitance_ performing both operations simultaneously.
+
+## Entity Diagram
+
+![class layout](./images/WaveShapers.png)
--- src/test/test_shaper.php
+++ src/test/test_shaper.php
@@ -6,23 +6,57 @@ require_once '../Synth.php';
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-$oGenerator = new Signal\Generator\Sine();
-$oGenerator->setWaveShaper(new Signal\Generator\WaveShaper\FixedPhaseFeedback(1.0));
+// Test some generators
+$aGenerators = [
+    'sine'     => new Signal\Generator\Sine(-0.9, 0.9),
+    'square'   => new Signal\Generator\Square(-0.9, 0.9),
+    'saw_up'   => new Signal\Generator\SawUp(-0.9, 0.9),
+    'saw_down' => new Signal\Generator\SawDown(-0.9, 0.9),
+    'triangle' => new Signal\Generator\Triangle(-0.9, 0.9),
+];
 
-$iOneSecond = Signal\Context::get()->getProcessRate();
-$oOutput    = new Output\Wav;
+$aShapers = [
+    'vanilla'    => false,
+    'phase_0.25' => new Signal\Generator\WaveShaper\FixedPhaseFeedback(0.25),
+    'phase_0.50' => new Signal\Generator\WaveShaper\FixedPhaseFeedback(0.5),
+    'phase_0.75' => new Signal\Generator\WaveShaper\FixedPhaseFeedback(0.75),
+    'phase_1.00' => new Signal\Generator\WaveShaper\FixedPhaseFeedback(1.0),
+    'cap_0.25'   => new Signal\Generator\WaveShaper\FixedCapacitance(0.25),
+    'cap_0.50'   => new Signal\Generator\WaveShaper\FixedCapacitance(0.5),
+    'cap_0.95'   => new Signal\Generator\WaveShaper\FixedCapacitance(0.95),
+];
 
-$oOscillator = new Oscillator\Audio\Simple(
-    $oGenerator,
-    440
-);
+$iLength = (int)(Signal\Context::get()->getProcessRate() / 8);
 
-echo "Testing : ", $oOscillator, "\n";
-$oOutput->open('output/test_wave_shaper.wav');
+foreach ($aGenerators as $sGeneratorName => $oGenerator) {
+    foreach ($aShapers as $sShaperName => $oShaper) {
+        if ($oShaper) {
+            $oGenerator->setWaveShaper($oShaper);
+        }
 
-do {
-    $oOutput->write($oOscillator->emit());
-} while ($oOscillator->getPosition() < $iOneSecond);
-echo "End: ", $oOscillator, "\n\n";
+        $sFileName = sprintf(
+            'output/waveshaper/test_%s_%s.wav',
+            $sGeneratorName,
+            $sShaperName
+        );
 
-$oOutput->close();
+        $oOutput     = new Output\Wav;
+        $oOscillator = new Oscillator\Audio\Simple(
+            $oGenerator,
+            220
+        );
+
+        echo "Testing : ", $oOscillator, "\n";
+        $oOutput->open($sFileName);
+
+        do {
+            $oOutput->write($oOscillator->emit());
+        } while ($oOscillator->getPosition() < $iLength);
+        echo "End: ", $oOscillator, "\n\n";
+
+        $oOutput->close();
+
+    }
+}
+
+
